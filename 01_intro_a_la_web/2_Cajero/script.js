00a0b28aebcae12d8eed430f0bb09a16dfa4d839
Valentina Cordoba <valentina182803@gmail.com>
2023-02-20 15:14:31 -0500

    Cajero

--- 01_intro_a_la_web/2_Cajero/script.js
+++ 01_intro_a_la_web/2_Cajero/script.js
@@ -1,6 +1,6 @@
 
 
-var cuentas = [
+var accountsInfo = [
   { name: "Mali Becerri", balance: 200, number: 60755912778, pwd: 1234 },
   { name: "Gera Rodriguez", balance: 290, number: 60765912778, pwd: 5678 },
   { name: "Maui Stan", balance: 67, number: 60775912778, pwd: 9012 }
@@ -10,16 +10,16 @@ var name= "";
 var amount=0;
 
 
-document.getElementById("validarBtn").addEventListener("click", function () {
+document.getElementById("validateBtn").addEventListener("click", function () {
   event.preventDefault();
   var label1 = parseInt(document.getElementById('account').value)
   var label2 = parseInt(document.getElementById('password').value)
   var control= false
 
-  for (var i = 0; i < cuentas.length; i++) {
-    if (cuentas[i].number === label1 && cuentas[i].pwd === label2) {
+  for (var i = 0; i < accountsInfo.length; i++) {
+    if (accountsInfo[i].number === label1 && accountsInfo[i].pwd === label2) {
       control=true;
-      window.location.href = "Profile.html?name="+cuentas[i].name+"&balance="+cuentas[i].balance+"&number="+cuentas[i].number;
+      window.location.href = "Profile.html?name="+accountsInfo[i].name+"&balance="+accountsInfo[i].balance+"&number="+accountsInfo[i].number;
       break;
     }
   }
